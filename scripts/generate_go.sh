dbabac2f7933c3067977e53325b851b6935e3de8
SnackOverflowError <>
2024-12-28 15:18:02 -0500

    added golang unit test

--- scripts/generate_go.sh
+++ scripts/generate_go.sh
@@ -4,10 +4,10 @@
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 
 # make the generated folder if it doesnt already exist
-mkdir ../generated
+mkdir -p generated
 
 protoc \
---proto_path=../messages \
---go_out=../generated \
+--proto_path=messages \
+--go_out=generated \
 --go_opt=paths=source_relative \
 metrics/node_stats.proto common/common.proto pump/pump_stats.proto pump/mixing_stats.proto
