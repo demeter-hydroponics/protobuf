472e666d45eab11f8a4f786a970c32826a833546
Aaditya Chaudhary <32117362+SnackkOverflowError@users.noreply.github.com>
2025-03-08 12:04:48 -0500

    some qol fixes (#23)
    Co-authored-by: SnackOverflowError <>

--- scripts/generate_python.sh
+++ scripts/generate_python.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-# protoc needs this to build go code
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-export PATH="$PATH:$(go env GOPATH)/bin"
-
 # make the generated folder if it doesnt already exist
-mkdir -p generated/go
+mkdir -p generated/python
 
 protoc \
 --proto_path=messages \
---go_out=generated/go \
---go_opt=paths=source_relative \
+--python_out=generated/python \
+--pyi_out=generated/python \
 common.proto \
 node/metrics.proto node/commands.proto \
 column/pump_metrics.proto column/mixing_metrics.proto column/pump_manager_info.proto column/commands.proto column/binary_load.proto \
